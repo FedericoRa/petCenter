c1c549194b9109eaf8c250f7511f61f56118f8ee
FedericoRa <fera.ramirez@gmail.com>
2021-05-15 17:16:58 -0500

    agrega el rpoyecto

---
+++ src/model/Status.java
@@ -0,0 +1,11 @@
+package model;
+
+
+/**
+*This enum class establishes a status of consult for each pet registered.
+*/
+
+public enum Status
+{
+	WAITING, IN_CONSULTATION, HOSPITALIZATION, DEPARTURE, DEPARTURE_WHITOUT_ATTENTION;  //it is the option of the status of the pet
+}
